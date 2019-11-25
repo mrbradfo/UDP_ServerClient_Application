2b6a181aef79bd4d1bc8368ad0e58aebdf40b877
mrbradfo <mrbradfo@gmail.com>
2019-11-25 14:01:33 -0700

    updated

--- UDP_ServerClient_Application/src/com/company/UDP_Server.java
+++ UDP_ServerClient_Application/src/com/company/UDP_Server.java
@@ -1,61 +1,43 @@
 package com.company;
 
-// Implementation using DatagramSocket
 import java.io.*;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
+import java.net.*;
 import java.util.LinkedList;
 
 public class UDP_Server {
 
     static LinkedList<UserData> userList = new LinkedList<UserData>();
 
+    private static final String ip = "127.0.0.1";
+    private static final int port = 32000;
+
+    private static final int STATE_OFFLINE = 0;
+    private static final int STATE_LOGIN_SENT = 1;
+    private static final int STATE_ONLINE = 2;
+
+    private static final int EVENT_USER_LOGIN = 0;
+    private static final int EVENT_USER_POST = 1;
+    private static final int EVENT_USER_INVALID = 79;
+    private static final int EVENT_NET_LOGIN_SUCCESSFUL = 80;
+    private static final int EVENT_NET_POST_ACK = 81;
+    private static final int EVENT_NET_INVALID = 255;
+
+    private static final int OPCODE_RESET = 0x00;
+    private static final int OPCODE_MUST_LOGIN_FIRST_ERROR = 0xF0;
+    private static final int OPCODE_LOGIN = 0x10;
+
+
     public static void main(String[] args) throws IOException {
-        // write your code here
         System.out.println("Hello this is the UDP Server!");
 
-        // Step 1 : Create a socket to listen at port 1234
-        DatagramSocket ds = new DatagramSocket(1234);
+        load();
+
+        // create a socket to listen at port 32000
+        DatagramSocket ds = new DatagramSocket(32000);
         byte[] receive = new byte[65535];
 
-        DatagramPacket DpReceive = null;
-
-
-        while (true)
-        {
-
-            // Step 2 : create a DatgramPacket to receive the data.
-            DpReceive = new DatagramPacket(receive, receive.length);
-
-            // Step 3 : revieve the data in byte buffer.
-            ds.receive(DpReceive);
-            StringBuilder clientMsg = data(receive);
-
-            if (clientMsg.toString().equals("clr")) {
-                userList.clear();
-                UDP_Client.save();
-            } else if (clientMsg.toString().equals("disp")) {
-                UDP_Client.dispUserList();
-            }
-
-
-//            System.out.println("Client:- " + clientMsg);
-
-
-
-            // Exit the server if the client sends "bye"
-            if (clientMsg.toString().contains("logout#"))
-            {
-                System.out.println("Client sent bye.....EXITING");
-                userList.remove(0);
-                System.out.println(userList.get(0).getUsername() + " removed");
-                break;
-            } else if (clientMsg.toString().contains("login#")) {
-                login(clientMsg);
-            }
-
+        while (true) {
+            receivePacket(ds, receive);
             // Clear the buffer after every message.
             receive = new byte[65535];
         }
@@ -64,8 +46,7 @@ public class UDP_Server {
 
     // A utility method to convert the byte array
     // data into a string representation.
-    public static StringBuilder data(byte[] a)
-    {
+    public static StringBuilder data(byte[] a) {
         if (a == null)
             return null;
         StringBuilder ret = new StringBuilder();
@@ -78,8 +59,73 @@ public class UDP_Server {
         return ret;
     }
 
-    public static boolean login(StringBuilder msg)
-    {
+    public static void sendPacket(String msg) throws IOException {
+
+        // create the socket object for
+        // carrying the data.
+        DatagramSocket ds = new DatagramSocket();
+        byte buf[] = null;
+
+        // convert the String input into the byte array.
+        buf = msg.getBytes();
+
+        // create the datagramPacket for sending
+        // the data.
+        DatagramPacket DpSend = new DatagramPacket(buf, buf.length, InetAddress.getByName(ip), port);
+
+        // invoke the send call to actually send
+        // the data.
+        ds.send(DpSend);
+    }
+
+    public static void receivePacket(DatagramSocket ds, byte[] receive) throws IOException {
+
+        DatagramPacket DpReceive = null;
+        // create a DatgramPacket to receive the data.
+        DpReceive = new DatagramPacket(receive, receive.length);
+
+        // revieve the data in byte buffer.
+        ds.receive(DpReceive);
+        StringBuilder clientMsg = data(receive);
+
+        System.out.println("Client msg: " + clientMsg);
+        String username = "";
+        String pass = "";
+
+        if (clientMsg.toString().contains("#") && clientMsg.toString().contains("&")) {
+            username = clientMsg.substring(clientMsg.indexOf("#") + 1, clientMsg.indexOf("&"));
+            pass = clientMsg.substring(clientMsg.indexOf("&") + 1);
+        }
+
+
+        if (clientMsg.toString().equals("clr")) {
+            System.out.println("Clearing userList...");
+            userList.clear();
+            save();
+        } else if (clientMsg.toString().equals("disp")) {
+            dispUserList();
+        } else if (clientMsg.toString().contains("logout#")) {
+            System.out.println("logging out " + username);
+        } else if (clientMsg.toString().contains("login#")) {
+            if (login(clientMsg)) {
+                System.out.println("Logged in!");
+            } else {
+                System.out.println("Log in failed...");
+            }
+        } else if (clientMsg.toString().contains("addusr#")) {
+//            String username = clientMsg.substring(clientMsg.indexOf("#") + 1, clientMsg.indexOf("&"));
+//            String pass = clientMsg.substring(clientMsg.indexOf("&") + 1);
+            UserData usr = new UserData(username, pass);
+            userList.add(usr);
+            save();
+            System.out.println("\n" + username + " has been added!\n");
+        }
+
+
+
+    }
+
+    public static boolean login(StringBuilder msg) {
         boolean loginSuccesful = false;
         String username = "";
         String pass = "";
@@ -101,12 +147,9 @@ public class UDP_Server {
         for (int i = 0; i < userList.size(); i++) {
             if(userList.get(i).getUsername().equals(username) &&  userList.get(i).getPassword().equals(pass))
             {
-                UserData usr = new UserData(username, pass);
-                userList.add(usr);
+                System.out.println("Log in Success!");
                 loginSuccesful = true;
-                UDP_Client.save();
-                System.out.println("\n" + username + " has been logged in!\n");
-                break;userList
+                break;
             }
 //            System.out.println("Stored usr: " + userList.get(i).getUsername());
 //            System.out.println("Stored pass: " + userList.get(i).getPassword());
@@ -114,7 +157,6 @@ public class UDP_Server {
 
         return  loginSuccesful;
     }
-
 
     public static void load() {
         try {
@@ -129,7 +171,7 @@ public class UDP_Server {
             fileIn.close();
             objectIn.close();
 
-            // return loadedList;
+//         return userList;
 
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
@@ -141,5 +183,36 @@ public class UDP_Server {
 
         return;
     }
+
+    public static void save() {
+
+        try {
+            FileOutputStream listFileOut = new FileOutputStream("savedUsers.txt");
+            ObjectOutputStream listObjectOut = new ObjectOutputStream(listFileOut);
+
+            listObjectOut.writeObject(userList);
+            listObjectOut.close();
+
+            System.out.println("userList was written to savedUsers.txt");
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void dispUserList() {
+        load();
+        System.out.println("\nDisplaying the list of all users!");
+        if (userList.isEmpty()) {
+            System.out.println("User list is empty!");
+        }
+        for (UserData userData : userList) {
+            System.out.println("-----------------");
+            System.out.println("User: " + userData.getUsername());
+            System.out.println("Pass: " + userData.getPassword());
+        }
+
+    }
+
 
 }
